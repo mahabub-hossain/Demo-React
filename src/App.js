530ff619af9a7cbb35bbd829d00e713ee2d8dc44
Mahabub Hossain <you@example.com>
2019-07-11 15:52:15 +0600

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My first React App
+         Now i m in master branch
         </p>
         <a
           className="App-link"
